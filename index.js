26a01af70eab039ff68f4575748dd555202a768f
Omar Richards <omarrichards01@gmail.com>
2023-05-30 11:59:55 -0400

    objects lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = {name: "Omar", address:"123 Appleseed lane"};
+
+function updateEmployeeWithKeyAndValue(employees,key,value){
+
+  const newEmployee = { ...employees,key,value}
+  newEmployee[key] = value;
+  return newEmployee
+  
+ 
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employees,key,value) {
+  
+  employees.push[key] = value
+
+  return employee
+}
+
+
+function deleteFromEmployeeByKey(employee, key) {
+  
+  delete employee[key]
+  return employee
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  
+  delete employee[key]
+  return employee
+}
